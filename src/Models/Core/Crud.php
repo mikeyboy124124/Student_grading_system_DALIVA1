8e8de0dc55f8e2a9659f05bf91ca5f5137b3b3b9
mikeyboy124124 <mikeandrewdaliva@gmail.com>
2025-07-15 09:29:14 +0800

    First commit

---
+++ src/Models/Core/Crud.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Famoso\Gs\Core;
+
+interface Crud {
+public function create();
+public function read();
+public function update();
+public function delete();
+}
+
+
---
+++ src/Models/StudentModel.php
@@ -0,0 +1,14 @@
+<?php 
+
+namespace Famoso\Gs\Model;
+use Famoso\Gs\Core\Crud;
+class StudentModel implements Crud{
+    public function create(){
+    }
+    public function read(){
+    }
+    public function update(){
+    }
+    public function delete(){
+    }
+}
